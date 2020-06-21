2ea9b413c40e0a207e79fa39adb227c4e14e5ef9
XuTongle <xutongle@gmail.com>
2020-06-21 17:39:14 +0800

    删除无用

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -18,9 +18,7 @@ Route::group([
      * 设置中心
      */
     $router->get('settings/system', 'SettingsController@system')->name('admin.settings.system');
-    $router->get('settings/user', 'SettingsController@user')->name('admin.settings.user');
     $router->get('settings/storage', 'SettingsController@storage')->name('admin.settings.storage');
-    $router->get('settings/socialite', 'SettingsController@socialite')->name('admin.settings.socialite');
 
     //用户
     $router->get('user/settings/basic', 'User\SettingsController@basic')->name('admin.user.settings.basic');
