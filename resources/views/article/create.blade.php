5750cfcdbd24edb2603a6581498fd7dbb0c00a2f
XuTongle <xutongle@gmail.com>
2020-08-13 02:22:05 +0800

    删除无用

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -2,64 +2,16 @@
 
 @section('title', __('Create Articles'))
 
-@push('footer')
-    <link href="{{ asset('vendor/summernote/summernote-bs4.min.css') }}" rel="stylesheet">
-    <script src="{{ asset('vendor/summernote/summernote-bs4.min.js') }}"></script>
-    <script src="{{ asset('vendor/summernote/lang/summernote-zh-CN.min.js') }}"></script>
-    <link href="{{ asset('vendor/select2/css/select2.min.css') }}" rel="stylesheet">
-    <script src="{{ asset('vendor/select2/js/select2.min.js') }}"></script>
-    <script src="{{ asset('vendor/select2/js/i18n/zh-CN.js') }}"></script>
-    <script>
-        $(document).ready(function () {
-            $('#content').summernote({
-                lang: 'zh-CN',
-                height: 350,
-                placeholder: '撰写文章',
-            });
-            $('#tag_values').select2({
-                placeholder: "选择话题",
-                minimumInputLength: 1,
-                maximumSelectionLength: 5,
-                multiple: true,
-                tags: true,
-                allowClear: true,
-                ajax: {
-                    url: '{{route('tag.auto-complete')}}',
-                    dataType: 'json',
-                    delay: 250,
-                    data: function (params) {
-                        var query = {
-                            query: params.term,
-                            limit: 15
-                        }
-                        return query;
-                    },
-                    processResults: function (data) {
-                        var data = $.map(data, function (obj) {
-                            obj.id = obj.name;
-                            obj.text = obj.name;
-                            return obj;
-                        });
-                        return {
-                            results: data
-                        };
-                    },
-                }
-            });
-        });
-    </script>
-@endpush
-
 @section('content')
     <div class="container">
-        <nav class="breadcrumb" aria-label="breadcrumb">
-            您现在的位置：
-            <a class="breadcrumb-item" href="{{ url('/')}}">{{ __('Home') }}</a>
-            <a class="breadcrumb-item" href="{{ route('article.index') }}">{{ __('Articles') }}</a>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{ url('/')}}">{{ __('Home') }}</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('article.index') }}">{{ __('Articles') }}</a></li>
             <span class="breadcrumb-item active">{{ __('Create Articles') }}</span>
-        </nav>
+        </ol>
+
         <div class="row">
-            <div class="col-md-12 main widget-form">
+            <div class="col-md-12 widget-form">
                 <form id="article_form" method="POST" role="form" enctype="multipart/form-data"
                       action="{{ route('article.store') }}">
                     @csrf
@@ -77,9 +29,9 @@
 
                     <div class="form-group">
                         <label for="thumbnail">文章封面</label>
-                        <input type="file" class="form-control-file @error('thumb') is-invalid @enderror" name="thumb" id="thumb">
+                        <input type="file" class="form-control-file @error('thumb_path') is-invalid @enderror" name="thumb_path" id="thumb_path">
                         <small id="thumbHelp" class="form-text text-muted">建议尺寸200*120</small>
-                        @error('thumb')
+                        @error('thumb_path')
                         <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -90,7 +42,7 @@
                         <div class="form-group col-md-4">
                             <select id="category_id" class="form-control @error('category_id') is-invalid @enderror" name="category_id">
                                 <option value="0">请选择分类</option>
-                                {!! \App\Models\Category::makeOptionTree(old('category_id')) !!}
+                                {!! \App\Models\ArticleCategory::makeOptionTree(old('category_id')) !!}
                             </select>
                             @error('category_id')
                             <span class="invalid-feedback" role="alert">
