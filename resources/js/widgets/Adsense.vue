ed658ed4684ac0b6d2ac390592fff00c7b9a66a7
XuTongle <xutongle@gmail.com>
2020-12-21 09:13:01 +0800

    adsense

---
+++ resources/js/widgets/Adsense.vue
@@ -0,0 +1,59 @@
+<template>
+    <div :id="aid">
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        adClient: {
+            type: String,
+            required: true
+        },
+        adSlot: {
+            type: String,
+            required: true
+        },
+        adFormat: {
+            type: String,
+            required: false,
+            default: 'auto'
+        },
+        adStyle: {
+            type: String,
+            required: false,
+            default: 'display: block'
+        },
+    },
+    data() {
+        return {
+            timer: '',
+            value: 0,
+            aid: ''
+        };
+    },
+    created() {
+        this.aid = 'as_' + this.adSlot;
+    },
+    mounted() {
+        this.setAdsense();
+        this.timer = setInterval(this.refreshAdsense, 300000);
+    },
+    methods: {
+        refreshAdsense() {
+            if (this.value < 999) {
+                this.value++;
+                this.setAdsense();
+            }
+        },
+        setAdsense() {
+            document.getElementById(this.aid).innerHTML = '<ins class="adsbygoogle" style="display:block" data-ad-client="' + this.adClient + '" data-ad-slot="' + this.adSlot + '" data-ad-format="auto"  data-full-width-responsive="true"></ins>';
+            (window.adsbygoogle = window.adsbygoogle || []).push({})
+        }
+    },
+    beforeDestroy() {
+        clearInterval(this.timer);
+    }
+}
+</script>
